f8e2e168c3ddd6ff6e76468d81ad40d56207314e
matr1xp <marlonsantos.au@gmail.com>
2021-09-11 13:21:18 +1000

    Initial commit

---
+++ windows/rndemo/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rndemo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rndemo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
