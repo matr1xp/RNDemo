651ce806bc9f4b0141e9f4eb279f7c5a590ff296
matr1xp <marlonsantos.au@gmail.com>
2021-09-11 20:18:56 +1000

    Eslint settings, .gitignore some files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+    semi: ['error', 'never'],
+  },
+}
